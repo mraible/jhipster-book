2c9b7976e71a1b4ff3e30eb437f70bd058122f56
Matt Raible <matt@raibledesigns.com>
2015-04-16 22:37:02 -0600

    JHIPSTER-14: Wrote BrowserSync section and figured out how to include source code using AsciiDoc's :import.

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        watch: {
+            web: {
+                files: ['src/main/webapp/css/*.css', 'src/main/webapp/js/**/*.js']
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'src/main/webapp/index.html',
+                        'src/main/webapp/tpl/**/*.html',
+                        'src/main/webapp/css/*.css',
+                        'src/main/webapp/js/**/*.js',
+                        'src/main/webapp/img/**/*.{png,jpg,jpeg,gif,webp,svg}'
+                    ]
+                }
+            },
+            options: {
+                watchTask: true,
+                server: {
+                    baseDir: "./src/main/webapp/"
+                }
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                'src/main/webapp/js/**/*.js'
+            ]
+        }
+    });
+
+    grunt.registerTask('serve', [
+        'browserSync',
+        'watch'
+    ]);
+
+    grunt.registerTask('default', [
+        'serve'
+    ]);
+};
