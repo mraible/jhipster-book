c20c63045ee73c1bc3d927af8ece8ac9c37c95d9
Matt Raible <matt@raibledesigns.com>
2015-05-06 23:23:18 -0600

    JHIPSTER-13: Finished first draft of WebSockets and closed out UI Components.

---
+++ src/main/webapp/chapter3/websockets/tracker.service.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('blogApp')
+    .factory('Tracker', function ($rootScope) {
+        var stompClient = null;
+
+        function sendActivity() {
+            stompClient.send('/websocket/activity', {},
+                JSON.stringify({'page': $rootScope.toState.name}));
+        }
+
+        return {
+            connect: function () {
+                var socket = new SockJS('/websocket/activity');
+                stompClient = Stomp.over(socket);
+                stompClient.connect({}, function(frame) {
+                    sendActivity();
+                    $rootScope.$on('$stateChangeStart', function (event) {
+                        sendActivity();
+                    });
+                });
+            },
+            sendActivity: function () {
+                if (stompClient != null) {
+                    sendActivity();
+                }
+            },
+            disconnect: function() {
+                if (stompClient != null) {
+                    stompClient.disconnect();
+                    stompClient == null;
+                }
+            }
+        };
+    });
