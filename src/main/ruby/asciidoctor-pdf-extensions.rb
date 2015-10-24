c675dd872f907a5bbe7cad1f11a554a0dca57271
mraible <matt@raibledesigns.com>
2015-10-24 17:38:52 -0600

    Figured out how to change the font size and color. Still don't know how to add a top margin to dedication and acknowledgement.
    Former-commit-id: 610672b9719c10d746ab90d6e3b4a2742897450d

--- src/main/ruby/asciidoctor-pdf-extensions.rb
+++ src/main/ruby/asciidoctor-pdf-extensions.rb
@@ -6,10 +6,10 @@ module AsciidoctorPdfExtensions
   end
 
   def layout_chapter_title node, title
-    if (node.id == "dedication" || node.id == "acknowledgements")
+    if node.id == "dedication" || node.id == "acknowledgements"
       # todo: Add underline of forward-slashes under title
       # todo: Add margin below title, before text
-      layout_heading title, align: :center, margin_top: 3
+        layout_heading_custom title, align: :center
     elsif node.id.include? "mini-book" # colophon
       # todo: make title font-size same as text, align with bottom of page
       layout_heading title, margin: 5
@@ -22,6 +22,25 @@ module AsciidoctorPdfExtensions
        # delegate to default implementation
        super
     end
+  end
+
+  def layout_heading_custom string, opts = {}
+      top_margin = (margin = (opts.delete :margin)) || (opts.delete :margin_top) || @theme.heading_margin_top
+      bot_margin = margin || (opts.delete :margin_bottom) || @theme.heading_margin_bottom
+      puts 'top margin: ' + top_margin.to_s
+
+      move_down (@theme.title_page_title_margin_top || 0)
+
+      if (transform = (opts.delete :text_transform) || @text_transform)
+          string = transform_text string, transform
+      end
+      margin_top top_margin
+      typeset_text string, calc_line_metrics((opts.delete :line_height) || @theme.heading_line_height), {
+          color: '002EB8', size: 40,
+          inline_format: true,
+          align: :left
+      }.merge(opts)
+      margin_bottom bot_margin
   end
 end
 
