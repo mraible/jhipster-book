65ac75e877777d12fd2c4d7e1446d1d213792776
mraible <matt@raibledesigns.com>
2015-10-25 20:39:21 -0600

    Started working on section pages. Still a ways to go.
    Former-commit-id: bab734d7a9d0f73a53c3eac8edb245085c7c4fa6

--- src/main/ruby/asciidoctor-pdf-extensions.rb
+++ src/main/ruby/asciidoctor-pdf-extensions.rb
@@ -8,16 +8,18 @@ module AsciidoctorPdfExtensions
   def layout_chapter_title node, title
     if node.id == "dedication" || node.id == "acknowledgements"
       # todo: Add underline of forward-slashes under title
-      # todo: Add margin below title, before text
-        layout_heading_custom title, align: :center
+      layout_heading_custom title, align: :center
     elsif node.id.include? "mini-book" # colophon
       # todo: make title font-size same as text, align with bottom of page
-      move_down 500
+      move_down 450
       layout_heading title, size: @theme.base_font_size
     elsif node.id.include? "jhipster" #chapters
       puts node.id
+      move_down 200
+      layout_heading 'Part', align: :right, size: 100, color: 'blue', font_family: 'Helvetica', bottom_margin: 0
+      layout_heading 'One', align: :right, size: 100, color: 'green', font_family: 'Helvetica', margin_bottom: 0
       # todo: add 'Part One|Two|Three' to title and make font name, size and colors match InfoQ
-      layout_heading title, align: :right, margin_top: 3
+      layout_heading title, align: :right, color: 'green'
       start_new_page
     else
        # delegate to default implementation
